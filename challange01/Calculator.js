f2e75da5ab2d88df9166f795f25dbd6bb4b73725
Md Refat Hossain <refatibnanwar@gmail.com>
2025-05-27 01:04:53 +0800

    calculator functionality done

--- challange01/Calculator.js
+++ challange01/Calculator.js
@@ -1,20 +1,54 @@
-import { Text, View, TextInput, StyleSheet, Button } from "react-native";
+import { Text, View, TextInput, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "./components/PrimaryButton";
+import { useState } from "react";
 
 function Calculator() {
-  function resetInputhandler() {}
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
+  const [result, setResult] = useState("");
 
-  function sumHandler() {}
+  function firstNumberInpurHandler(enteredNumber) {
+    setFirstNumber(enteredNumber);
+  }
+
+  function secondNumberInputHandler(enteredNumber) {
+    setSecondNumber(enteredNumber);
+  }
+
+  function resetInputhandler() {
+    setFirstNumber("");
+    setSecondNumber("");
+    setResult("");
+  }
+
+  function sumHandler() {
+    const fNumber = parseFloat(firstNumber);
+    const sNumber = parseFloat(secondNumber);
+
+    if (isNaN(fNumber) || isNaN(sNumber)) {
+      Alert.alert("Invalid Number!", "Please input a numeric value", [
+        { text: "Okay", style: "destructive", onPress: resetInputhandler },
+      ]);
+      return;
+    }
+    var sum = fNumber + sNumber;
+    setResult(sum.toFixed(2));
+    console.log(sum);
+  }
 
   return (
     <View>
       <TextInput
         style={styles.textInput}
         placeholder="first number"
+        onChangeText={firstNumberInpurHandler}
+        value={firstNumber}
       ></TextInput>
       <TextInput
         style={styles.textInput}
         placeholder="second number"
+        onChangeText={secondNumberInputHandler}
+        value={secondNumber}
       ></TextInput>
       <View style={styles.buttonsContainer}>
         <View style={styles.singleButtonContainer}>
@@ -24,7 +58,7 @@ function Calculator() {
           <PrimaryButton onPress={sumHandler}>Add</PrimaryButton>
         </View>
       </View>
-      <Text style={styles.textStyle}>Total</Text>
+      <Text style={styles.textStyle}>Total is {result} </Text>
     </View>
   );
 }
@@ -37,7 +71,7 @@ const styles = StyleSheet.create({
     padding: 8,
     color: "#4e0329",
     fontSize: 16,
-    height: 32,
+    height: 36,
     width: "300",
     borderBottomWidth: 2,
     borderBottomColor: "#4e0329",
