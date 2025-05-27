c3422ccea2c1f83ea705f0240487a98671957bf5
Md Refat Hossain <refatibnanwar@gmail.com>
2025-05-27 16:29:35 +0800

    Challange 03 done.

---
+++ challange03/challange03.js
@@ -0,0 +1,22 @@
+function twoSum(numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    if (numbers[left] + numbers[right] > target) {
+      //console.log(numbers[left] + numbers[right])
+      right -= 1;
+    } else if (numbers[left] + numbers[right] < target) {
+      left += 1;
+    } else {
+      // console.log(left);
+      // console.log(right);
+      return [left + 1, right + 1];
+    }
+  }
+  return [];
+}
+
+console.log(twoSum([4, 11, 17, 25], 21));
+console.log(twoSum([0, 1, 2, 2, 3, 5], 4));
+console.log(twoSum([-1, 0], -1));
